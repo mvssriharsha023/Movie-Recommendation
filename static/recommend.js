f78c27918c2bc27a79edf3a10f96a22c5c227fe6
mvssriharsha023 <110189826+mvssriharsha023@users.noreply.github.com>
2024-03-22 22:22:34 +0530

    Add files via upload

---
+++ static/recommend.js
@@ -0,0 +1,11 @@
+const showSidebar = () => {
+    let menuSideBar = document.querySelector(".menu-sidebar");
+    menuSideBar.style.display = "flex";
+    
+}
+
+const hideSidebar = () => {
+    let menuSideBar = document.querySelector(".menu-sidebar");
+    menuSideBar.style.display = "none";
+    
+}
